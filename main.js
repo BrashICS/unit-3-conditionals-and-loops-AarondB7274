1c57084015f834324188c1224fd8b5d0e6b9dac3
Aaron de Boer <aaron.deboer@stu.ocsb.ca>
2024-10-30 11:55:30 -0400

    Finished part 1

--- main.js
+++ main.js
@@ -29,3 +29,37 @@ function randInt(min, max) {
 }
 
 // For "equal to" use == , for "and" use && , for  "or" use ||
+
+function user() {
+    let user_age = Number(prompt(`How old are you?`))
+    if (user_age >= 60) {
+        console.log(`You qualify for a senior discount!`)
+    }
+    if (user_age < 16) {
+        console.log(`You aren't old enough to drive yet! No worries, I'm old enough and don't know anyways.`)
+    }
+    if (user_age == 44) {
+        console.log(`You're the same age as Mr. Brash, cool!`)
+    }
+    let user_name = prompt(`What's your prefered name (or tag)?`)
+    if (user_name == "Mr. Squirrel") {
+        console.log(`Are you a 🐿️?`)
+    }
+    let name_length = user_name.length
+    if(name_length < 4) {
+        console.log(`You have an under average length name!`)
+    }
+    if(name_length < 8) {
+        console.log(`You have an above average length name!`)
+    }
+    let name_guess = Number(prompt(`How many character do you think that was?`))
+    if(name_guess = name_length) {
+        console.log(`Thats right! Your name is ${name_length} characters long! Not the hardest guess, if I'm honest.`)
+    }
+    if(name_guess > name_length) {
+        console.log(`Sorry, thats too many characters. No worries, string length can be difficult.`)
+    }
+    if(name_guess < name_length) {
+        console.log(`Sorry, thats too few characters. No worries, string length can be difficult.`)
+    }
+}
