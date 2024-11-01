0efb261dfa793e6033817e17d7b6ef1b098bb11c
Aaron de Boer <aaron.deboer@stu.ocsb.ca>
2024-11-01 13:23:14 -0400

    fixed timing

--- main.js
+++ main.js
@@ -63,10 +63,10 @@ function user() {
         console.log(`Sorry, thats too few characters. No worries, string length can be difficult.`)
     }
 }
-
+let panic = 0
+let time = 0
+let loop = 0
 function start_game() {
-    let time = 0
-    let loop = 0
     let user_name = prompt(`Hello there, what's you name?`)
     let user_colour = prompt(`Now I just need a colour, any colour would work but I suggest your favourite.`)
     alert(`Welcome, ${user_name}, to a slightly spooky (and *really* mentaly ill) little game!`)
@@ -76,45 +76,51 @@ function start_game() {
 }
 
 function start_choice() {
+    time = 0
+    cover = false
+    loop = loop + 1
+    if(panic > 0) {
+        panic = panic - 1
+    }
     let first_choice = Number(prompt(`Where do you want to go, the forest (1), the sea (2), the ruins (3) or the mountains (4)?`))
-    if(first_choice=1) {
+    if(first_choice = 1) {
         forest()
-        }
-    if(first_choice=2) {
+        } else if(first_choice == 2) {
         sea()
-        }
-    if(first_choice=3) {
+        } else if(first_choice == 3) {
         ruins()
-        }
-    if(first_choice=4) {
+        } else if(first_choice == 4) {
         mountains()
         }
     return time
 }
 function forest() {
-    alert(`You head to the forest. The trees and undergrowth are blanketed in a thick layer of snow. Its all quite and you venture farther in, the snow crunching beneath your feet. After a while you hear erie giggling and soundss of struggle in the distance.`)
+    alert(`You head to the forest. The trees and undergrowth are blanketed in a thick layer of snow. Its all quite and you venture farther in, the snow crunching beneath your feet. After a while you hear erie giggling and sounds of struggle in the distance.`)
     let second_choice = Number(prompt(`Do you want to stand your ground (1), hide (2) or run (3)?`))
-    if (second_choice = 1) {
+    if (second_choice == 1) {
         forest_stand()
     }
-    if (second_choice = 2) {
+    if (second_choice == 2) {
         forest_hide()
     }
-    if (second_choice = 3) {
+    if (second_choice == 3) {
         forest_run()
     }
     return time
 }
 
 function forest_stand() {
-        alert(`You stand your ground. The noises continue for quite some time, often punctuated with screams. A figure is launched above the canopy in the distance, and the forest goes silent as another follows.`)
-        time = time + 0.5
-        return time
+        alert(`You stand your ground. The noises continue for quite some time, often punctuated with screams. A winged figure is launched above the canopy in the distance, and the forest goes silent as another follows.`)
+        forest_second()
+        time = time + 1
+        panic = panic + 1
+        return time, panic
 }
 
 function forest_hide() {
         alert(`You hide in the undergrowth. The noises continue for quite some time, often punctuated with screams. The forest soon goes silent but you stay hidden longer out of fear, when you come out the moons have falled behind the trees.`)
-        time = time + 1.5
+        forest_second()
+        time = time + 3
         return time
 }
 
@@ -122,22 +128,28 @@ function forest_run() {
         alert(`You run away. As you stumble over the snow covered roots and stones, a figure lands on a tree infront of you, alerted to your presence by yout noisy retreat. You manage to let out a single, bone chilling scream before they lung at you, a mass of tatered wings and metal.`)
         alert(`The snow is stained black with your spilled oil, not the most pleasant end.`)
         forest()
+        time = 0
         return time
 }
 
 function forest_second() {
     alert(`You stand in the forest, the snow faintly lit ${user_colour} from the glow of your eyes.`)
-    let third_choice = Number(prompt(``))
+    let third_choice = Number(prompt(`Do you want to venture deeper into the forest (1), or go back (2)?`))
+    if(third_choice == 1) {
+        forest_continue()
+    } else if(third_choice == 2) {
+        leave()
+    }
 }
 
 function sea() {
     alert(`You head to the seaside. The ice is perfectly smooth, a glassy mirror reflecting the moons from above. The shore is blanketed in snow, bordering the blue sea in white. It's calm here, a pair of winged figures dance in the sky over the frozen waters.`)
     let second_choice = Number(prompt(`Do you want to stare out across the ice (1), watch the figures (2) or leave (3)?`))
-    if(second_choice = 1) {
+    if(second_choice == 1) {
         sea_stare()
-    } else if(second_choice = 2) {
+    } else if(second_choice == 2) {
         sea_watch()
-    } else if(second_choice = 3) {
+    } else if(second_choice == 3) {
         leave()
     }
     time = time + 0
@@ -147,29 +159,37 @@ function sea() {
 function sea_stare() {
     alert(`You stare out across the frozen sea. You stay for quite some time, when you rise the figures have gone and the moons have gone below the horizon.`)
     sea_second()
-    time = time + 1.5
-    return time
+    time = time + 3
+    if(panic > 1) {
+        panic = panic - 2
+    } else if(panic > 0) {
+        pnic = panic - 1
+    }
+    return time, panic
 }
 
 function sea_watch() {
     alert(`You watch the figures frolic in the sky. They zip around the clouds, decending with incredible speed before ascending again.`)
     sea_second()
-    time = time + 0.5
+    time = time + 1
+    if(panic > 0) {
+        panic = panic - 1
+    }
     return time
 }
 
 function leave() {
     alert(`You head back to the feild where you woke up. There wasn't anything that intrested you here anyways.`)
     start_choice()
-    time = time + 0.5
+    time = time + 1
     return time
 }
 
 function sea_second() {
     let third_choice = Number(prompt(`You can wait here longer (1) or go back to the start (2)`))
-    if(third_choice = 1) {
+    if(third_choice == 1) {
         sea_wait()
-    } else if(third_choice = 2) {
+    } else if(third_choice == 2) {
         sea_leave()
     }
     return time
@@ -177,12 +197,12 @@ function sea_second() {
 
 function sea_wait() {
     alert(`You stare you over the frozen waters. Its surface is perfectly smooth and still, not even a wave, frozen in time to be seen.`)
-    time = time + 0.5
+    time = time + 1
     return time
 }
 
 function ruins() {
-
+    alert(`You head towards the ruined city. The crumbling buildings tower overhead, their windows strewn about as fragments of glass. Broken rebar jut out from the ends of snapped skyscrapers. A head crunches beneath your feet, the message "FATAL ERROR" displayed beneath its glass face.`)
 }
 
 function mountains() {
@@ -190,47 +210,37 @@ function mountains() {
 }
 
 
-if(time = 0.5) {
-    time = time + 0.5
+if(time = 1) {
+    time = time + 1
     alert(`Its very early into the night, the moons have just risen above the eastern horizon.`)
-}
-
-if(time = 1.5) {
-    time = time + 0.5
+} else if(time == 3) {
+    time = time + 1
     alert(`Its early into the night, the moons hover just above the eastern horizon.`)
-}
-
-if(time = 2.5) {
-    time = time + 0.5
+} else if(time == 5) {
+    time = time + 1
     alert(`Its just before midnight, the moons hang overhead.`)
-}
-
-if(time = 3.5) {
-    time = time + 0.5
+} else if(time == 7) {
+    time = time + 1
     alert(`Its midnight, the moons hang directly above you in the sky.`)
-}
-
-if(time = 4.5) {
-    time = time + 0.5
+} else if(time == 9) {
+    time = time + 1
     alert(`Its just after midnight, the moons hang nearly overhead.`)
-}
-
-if(time = 5.5) {
-    time = time + 0.5
+} else if(time == 11) {
+    time = time + 1
     alert(`Its late into the night, the moons hang over the western horizon. You should find shelter before the morning.`)
-}
-
-if(time = 6.5) {
-    time = time + 0.5
+} else if(time == 13) {
+    time = time + 1
     alert(`Its very late into the night, the moons hang just above the western horizon. You should find shelter immediately.`)
-}
-
-if(time > 7, cover = false) {
+} else if(time > 14, cover == false) {
     alert(`The sun comes up over the horizon, its sickly yellow glare burning your skin. The smell of burning metal perforates the air around you as you are involitarily incinerated.`)
     start_choice()
     time = 0
-} else if(time > 7, cover = true) {
+} else if(time > 14, cover == true) {
     alert(`The sun comes up over the horizon, its sickly yellow glare casting an orange hue over the outside world. Your hiding place protects you from it. Nothing will come after you in the day, but you won't be able to go anywhere either.`)
     loop = loop + 1
     time = 0
+}
+
+if(panic > 7) {
+    alert(``)
 }
